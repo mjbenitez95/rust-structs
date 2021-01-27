1cbff53584e944b3a7a057bd8aa5976fdb627787
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-27 13:17:00 -0800

    implement methods for area and can_hold

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        (other.height <= self.height) && (other.width <= self.width)
+    }
+}
+
 fn area(width: u32, height: u32) -> u32 {
     width * height
 }
@@ -21,12 +31,47 @@ fn main() {
     let height = 50;
 
     let rectangle_tuple = (width, height);
-    let rectangle_struct = Rectangle { width, height};
+    let rectangle_struct = Rectangle { width, height };
 
-    println!("The area of the rectangle is {} square pixels.", area(width, height));
-    println!("The area of the rectangle is {} square pixels.", tuple_area(rectangle_tuple));
-    println!("The area of the rectangle is {} square pixels.", struct_area(&rectangle_struct));
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width, height)
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        tuple_area(rectangle_tuple)
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        struct_area(&rectangle_struct)
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rectangle_struct.area()
+    );
 
-    println!("The rectangle is {:?}", rectangle_struct);
-    println!("The rectangle is {:#?}", rectangle_struct);
+    println!("The contents of rectangle are: {:?}", rectangle_struct);
+    println!("The contents of rectangle are: {:#?}", rectangle_struct);
+
+    let rectangle_a = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rectangle_b = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rectangle_c = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!(
+        "It is {} that rectangle A can hold rectangle B.",
+        rectangle_a.can_hold(&rectangle_b)
+    );
+    println!(
+        "It is {} that rectangle A can hold rectangle C.",
+        rectangle_a.can_hold(&rectangle_c)
+    );
 }
