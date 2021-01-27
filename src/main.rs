93c8dafc7c1b038c8e5dff6111a741d75483ebc1
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-01-27 12:33:33 -0800

    implement area calculation three ways

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn tuple_area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn struct_area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width = 30;
+    let height = 50;
+
+    let rectangle_tuple = (width, height);
+    let rectangle_struct = Rectangle { width, height};
+
+    println!("The area of the rectangle is {} square pixels.", area(width, height));
+    println!("The area of the rectangle is {} square pixels.", tuple_area(rectangle_tuple));
+    println!("The area of the rectangle is {} square pixels.", struct_area(rectangle_struct));
 }
